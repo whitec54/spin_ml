19bc6a62d7b41718b4cfcd01c49ba2f0b7e70380
Christian Fulljames <christianfulljames@gmail.com>
2017-05-30 21:05:29 -0400

    Make a prettier topic selection list
    Remove all HTML formatting from the topic selection list, and provide a number
    for each link (to be used for selection in a later commit)

--- src/gather_data.py
+++ src/gather_data.py
@@ -5,6 +5,7 @@
 
 import getpass
 from robobrowser import RoboBrowser
+import re
 
 login_url = 'http://ezproxy.gvsu.edu/login?url=http://infoweb.newsbank.com'
 
@@ -13,14 +14,13 @@ def __main__():
 	display_welcome()
 	username, password = get_user_and_pass()
 	browser = login_to_gvsu(username, password)
-	selection = display_topics(browser)
+	topic_strings = display_topics(browser)
 
 # Display the welcome message
 def display_welcome():
-	print("""
-			Welcome to the GVSU NewsBank Scanner
-			Note: This program requires a valid GVSU student login
-	""")
+	print("Welcome to the GVSU NewsBank Scanner\n"
+			"Note: This program requires a valid GVSU student login")
+
 
 # Prompt the user for their login information
 def get_user_and_pass():
@@ -43,6 +43,11 @@ def display_topics(browser):
 	view_all_link = browser.get_link(text='View All Special Reports')
 	browser.follow_link(view_all_link)
 	links = browser.find_all('a')
-	print(links)
+	link_text = []
+	for i in range(0, len(links) - 1):
+		link_text[i] = re.sub('<.*?>', '', str(links[i]))
+		link_text[i] = re.sub('\n', '', link_text[i])
+		print(i, link_text[i])
+	return link_text
 
 __main__()
